18bfc017d6ce2e5fb5e18a3267bbf114894e189c
Pedro Nunes <pedro.nunes02@hotmail.com>
2023-09-22 13:54:03 -0300

    Feito os testes na classe

--- Classes/class.Paciente.php
+++ Classes/class.Paciente.php
@@ -1,5 +1,28 @@
+<?php
+
+include_once("../Testes/class.pessoa.teste.php");
+
 class Paciente extends Pessoa {
     protected $tratamento;
-    protected $dataDeNascimento;
     protected $responsavelFinanceiro;
+    protected $dataDeNascimento;
+
+    public function __construct($nome, $telefone, $email, $cpf, $rg, $tratamento, $dataDeNascimento, $responsavelFinanceiro) {
+        parent::__construct($nome, $telefone, $email, $cpf, $rg);
+        $this->tratamento = $tratamento;
+        $this->dataDeNascimento = $dataDeNascimento;
+        $this->responsavelFinanceiro = $responsavelFinanceiro;
+    }
+
+    public function getTratamento() {
+        return $this->tratamento;
+    }
+
+    public function getDataDeNascimento() {
+        return $this->dataDeNascimento;
+    }
+
+    public function getResponsavelFinanceiro() {
+        return $this->responsavelFinanceiro;
+    }
 }
