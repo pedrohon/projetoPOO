68ac208a7c20da0afb1eb7b25d2d9039634cbcac
Juliaffp <juliaffp25@gmail.com>
2023-09-29 14:37:47 -0300

    Create class.FormaDePagamento.php

---
+++ Classes/class.FormaDePagamento.php
@@ -0,0 +1,24 @@
+ <?php
+
+ class FormaDePagamento{
+
+ protected string $nomeFormaDePagamento;
+ protected int    $quantidadeDeParcelas;
+
+
+ public function __construct($nomeFormaDePagamento,$quantidadeDeParcelas){
+
+    $this->nomeFormaDePagamento=$nomeFormaDePagamento;
+    $this->quantidadeDeParcelas=$quantidadeDeParcelas;
+ }
+
+public function getNomeFormaDePagamento(){
+
+    return $this->nomeFormaDePagamento;
+}
+public function getquantidadeDeParcelas(){
+
+    return $this->quantidadeDeParcelas;
+}
+
+ }
