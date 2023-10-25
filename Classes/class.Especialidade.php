fee6328e145422c151543d19116b5449501a77c3
Sarah <sarahfernandadc@gmail.com>
2023-10-25 15:39:00 -0300

    Criando classe Especialidade

---
+++ Classes/class.Especialidade.php
@@ -0,0 +1,36 @@
+<?php
+
+include_once '../global.php';
+
+class Especialidade {
+
+  static $local_filename = "Especialidade.txt";
+  static public function getFilename() {
+    return get_called_class()::$local_filename;
+  }
+
+  protected $especialidade;
+  protected $procedimento = array();
+
+  public function __construct($especialidade, Procedimento $procedimento) {
+    $this->especialidade = $especialidade;
+    $this->procedimento = $procedimento;
+  }
+
+  public function getEspecialidade() {
+    return $this->especialidade;
+  }
+
+  public function getProcedimento() {
+    return $this->procedimento;
+  }
+ 
+  public function setEspecialidade($especialidade) {
+    $this->especialidade = $especialidade;
+  }
+ 
+  public function setProcedimento($procedimento) {
+    $this->procedimento = $procedimento;
+  }
+  
+}
