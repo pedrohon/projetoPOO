d858518167f8142a873af59bfb1734fe411cea9a
Sarah <sarahfernandadc@gmail.com>
2023-10-25 15:09:54 -0300

    Implentando o persist

--- Classes/class.Pessoa.php
+++ Classes/class.Pessoa.php
@@ -2,12 +2,10 @@
 
 include_once '../global.php';
 
-class Pessoa extends persist
-{
+class Pessoa extends persist {
 
   static $local_filename = "Pessoa.txt";
-  static public function getFilename()
-  {
+  static public function getFilename() {
     return get_called_class()::$local_filename;
   }
 
@@ -17,8 +15,7 @@ class Pessoa extends persist
   protected $cpf;
   protected $rg;
 
-  public function __construct($nome, $telefone, $email, $cpf, $rg)
-  {
+  public function __construct($nome, $telefone, $email, $cpf, $rg) {
     $this->nome = $nome;
     $this->telefone = $telefone;
     $this->email = $email;
@@ -26,53 +23,43 @@ class Pessoa extends persist
     $this->rg = $rg;
   }
 
-  public function getNome()
-  {
+  public function getNome() {
     return $this->nome;
   }
 
-  public function getTelefone()
-  {
+  public function getTelefone() {
     return $this->telefone;
   }
 
-  public function getEmail()
-  {
+  public function getEmail() {
     return $this->email;
   }
 
-  public function getCpf()
-  {
+  public function getCpf() {
     return $this->cpf;
   }
 
-  public function getRg()
-  {
+  public function getRg() {
     return $this->rg;
   }
 
-  public function setNome($nome)
-  {
+  public function setNome($nome) {
     $this->nome = $nome;
   }
 
-  public function setTelefone($telefone)
-  {
+  public function setTelefone($telefone) {
     $this->telefone = $telefone;
   }
 
-  public function setEmail($email)
-  {
+  public function setEmail($email) {
     $this->email = $email;
   }
 
-  public function setCpf($cpf)
-  {
+  public function setCpf($cpf) {
     $this->cpf = $cpf;
   }
   
-  public function setRg($rg)
-  {
+  public function setRg($rg) {
     $this->rg = $rg;
   }
 }
