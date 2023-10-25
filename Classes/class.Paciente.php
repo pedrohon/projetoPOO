4341931aea22cb82b8e53498c251adaa95f31c1f
Sarah <sarahfernandadc@gmail.com>
2023-10-25 15:17:19 -0300

    Implementando o persist

--- Classes/class.Paciente.php
+++ Classes/class.Paciente.php
@@ -3,26 +3,48 @@
 include_once '../global.php';
 
 class Paciente extends Pessoa {
-    protected $tratamento;
-    protected $responsavelFinanceiro;
-    protected $dataDeNascimento;
 
-    public function __construct($nome, $telefone, $email, $cpf, $rg, $tratamento, $dataDeNascimento, $responsavelFinanceiro) {
-        parent::__construct($nome, $telefone, $email, $cpf, $rg);
-        $this->tratamento = $tratamento;
-        $this->dataDeNascimento = $dataDeNascimento;
-        $this->responsavelFinanceiro = $responsavelFinanceiro;
-    }
+  static $local_filename = "Paciente.txt";
+  static public function getFilename() {
+    return get_called_class()::$local_filename;
+  }
 
-    public function getTratamento() {
-        return $this->tratamento;
-    }
+  protected $tratamento;
+  protected $responsavelFinanceiro;
+  protected $dataDeNascimento;
 
-    public function getDataDeNascimento() {
-        return $this->dataDeNascimento;
-    }
+  public function __construct($nome, $telefone, $email, $cpf, $rg, $tratamento, $dataDeNascimento, /*Cliente*/ $responsavelFinanceiro) {
+    parent::__construct($nome, $telefone, $email, $cpf, $rg);
+    $this->tratamento = $tratamento;
+    $this->dataDeNascimento = $dataDeNascimento;
+    $this->responsavelFinanceiro = $responsavelFinanceiro;
+  }
 
-    public function getResponsavelFinanceiro() {
-        return $this->responsavelFinanceiro;
-    }
+  public function getTratamento() {
+    return $this->tratamento;
+  }
+
+  public function getDataDeNascimento() {
+    return $this->dataDeNascimento;
+  }
+
+  public function getResponsavelFinanceiro() {
+    return $this->responsavelFinanceiro;
+  }
+
+  public function setTratamento($tratamento) {
+    $this->tratamento = $tratamento;
+  }
+
+  public function setDataDeNascimento($dataDeNascimento) {
+    $this->dataDeNascimento = $dataDeNascimento;
+  }
+
+  public function setResponsavelFinanceiro($responsavelFinanceiro) {
+    $this->responsavelFinanceiro = $responsavelFinanceiro;
+  }
+
+  public function salvarPaciente (){
+    $this->save();
+  }
 }
