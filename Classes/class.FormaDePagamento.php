3c235812b70c76fed8ac78f7f674078b8cd45fb9
Sarah <sarahfernandadc@gmail.com>
2023-10-26 13:20:31 -0300

    Implementando o persist

--- Classes/class.FormaDePagamento.php
+++ Classes/class.FormaDePagamento.php
@@ -1,26 +1,35 @@
- <?php
+<?php
 
 include_once '../global.php';
 
- class FormaDePagamento{
+class FormaDePagamento extends persist {
 
- protected string $nomeFormaDePagamento;
- protected int    $quantidadeDeParcelas;
+  static $local_filename = "FormaDePagamento.txt";
+  static public function getFilename() {
+    return get_called_class()::$local_filename;
+  }
 
+  protected $nomeFormaDePagamento;
+  protected $quantidadeDeParcelas;
 
- public function __construct($nomeFormaDePagamento,$quantidadeDeParcelas){
-
+  public function __construct($nomeFormaDePagamento,$quantidadeDeParcelas) {
     $this->nomeFormaDePagamento=$nomeFormaDePagamento;
     $this->quantidadeDeParcelas=$quantidadeDeParcelas;
- }
+  }
 
-public function getNomeFormaDePagamento(){
-
+  public function getNomeFormaDePagamento() {
     return $this->nomeFormaDePagamento;
-}
-public function getquantidadeDeParcelas(){
-
+  }
+  
+  public function getQuantidadeDeParcelas() {
     return $this->quantidadeDeParcelas;
-}
+  }
 
- }
+  public function setNomeFormaDePagamento($nomeFormaDePagamento) {
+    $this->nomeFormaDePagamento = $nomeFormaDePagamento;
+  }
+
+  public function setQuantidadeDeParcelas($quantidadeDeParcelas){
+    $this->quantidadeDeParcelas = $quantidadeDeParcelas;
+  }
+}
