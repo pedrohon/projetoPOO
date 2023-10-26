9eea02f42e6f910426b6f7ce961c049f07467925
Sarah <sarahfernandadc@gmail.com>
2023-10-26 13:34:16 -0300

    Configurando o processador de cadastros

--- cadastros/cadastroCliente.php
+++ cadastros/cadastroCliente.php
@@ -1,25 +1,15 @@
 <?php
-require_once('../classes/class.Cliente.php');
-require_once('classes/persist.php');
 
-// Receba informações do formulário
-$nomeCliente = $_POST['nome'];
-$rgCliente = $_POST['rg'];
-$emailCliente = $_POST['email'];
-$telefoneCliente = $_POST['telefone'];
-$cpfCliente = $_POST['cpf'];
-$pagamentoCliente = $_POST['pagamento'];
+include_once '../global.php';
 
-// Crie um objeto Cliente com as informações
-$cliente = new Cliente($nomeCliente, $rgCliente, $telefoneCliente, $emailCliente, $cpfCliente);
+    $nome = $_POST['nome'];
+    $telefone = $_POST['telefone'];
+    $email = $_POST['email'];
+    $cpf = $_POST['cpf'];
+    $rg = $_POST['rg'];
+    $tratamento = $_POST['tratamento']; 
+    $dataDeNascimento = $_POST['dataDeNascimento']; 
+    $responsavelFinanceiro = $_POST['responsavelFinanceiro'];
 
-// Aqui você pode definir o pagamento do cliente, conforme necessário
-$cliente->setPagamento($pagamentoCliente);
-
-// Salve o objeto Cliente no arquivo
-$cliente->save();
-
-// Redirecione para uma página de confirmação ou outra página
-header("Location: index.php"); // Altere o URL conforme necessário
-exit();
-?>
+    $paciente = new Paciente($nome, $telefone, $email, $cpf, $rg, $tratamento, $dataDeNascimento, $responsavelFinanceiro);
+    $paciente -> salvarPaciente();
