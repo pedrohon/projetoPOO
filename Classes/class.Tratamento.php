130638f45f0dde01947f277e11b9e9b9a3d5575c
Sarah <sarahfernandadc@gmail.com>
2023-10-26 13:16:26 -0300

    Implementadno o persist

--- Classes/class.Tratamento.php
+++ Classes/class.Tratamento.php
@@ -2,8 +2,25 @@
 
 include_once '../global.php';
 
-class Tratamento extends Orçamento {
-  
-}
+class Tratamento extends Orcamento {
 
-?>
+  static $local_filename = "Tratamento.txt";
+  static public function getFilename() {
+    return get_called_class()::$local_filename;
+  }
+
+  protected $formaDePagamento;
+
+  public function __construct(Paciente $paciente, Dentista $dentista, $data, Procedimento $procedimentos, $valorTotal, $aprovacao, FormaDePagamento $formaDePagamento) {
+    parent::__construct($paciente, $dentista, $data, $procedimentos, $valorTotal, $aprovacao);
+    $this->formaDePagamento = $formaDePagamento;
+  }
+
+  public function getFormaDePagamento() {
+    return $this->formaDePagamento;
+  }
+
+  public function setFormaDePagamento($formaDePagamento) {
+    $this->formaDePagamento = $formaDePagamento;
+  }
+}
