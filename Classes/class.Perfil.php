63d9e7f9725c0efea13ca459117991994679fe11
Júlia Silva <jujubatista_07@hotmail.com>
2023-12-03 21:04:01 -0300

    criada classe perfil

---
+++ Classes/class.Perfil.php
@@ -0,0 +1,38 @@
+<?php
+
+include_once("../Testes/class.pessoa.teste.php");
+
+class Perfil {
+    protected $id;
+    protected $nomeDoPerfil;
+    protected $funcionalidades = array();
+
+    public function __construct($id, $nomeDoPerfil, $funcionalidades) {
+        $this->id = $id;
+        $this->nomeDoPerfil = $nomeDoPerfil;
+        $this->funcionalidades = $funcionalidades;
+    }
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getNomeDoPerfil() {
+        return $this->nomeDoPerfil;
+    }
+
+    public function getFuncionalidades() {
+        return $this->funcionalidades;
+    }
+
+    public function adicionarFuncionalidades() {
+        $this->funcionalidades[] = $funcionalidade;
+    }
+
+    public function removerFuncionalidades() {
+        $key = array_search($funcionalidade, $this->funcionalidades);
+        if ($key !== false){
+          unset($this->funcionalidades[$key]);
+        }
+    }
+}
