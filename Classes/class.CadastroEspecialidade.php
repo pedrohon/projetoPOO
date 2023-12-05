1c571f706d64ab778615cb5bb82bbd3976fb6ca3
Sarah <114375054+sarahfernandadc@users.noreply.github.com>
2023-12-05 19:42:33 -0300

    Update class.CadastroEspecialidade.php

--- Classes/class.CadastroEspecialidade.php
+++ Classes/class.CadastroEspecialidade.php
@@ -4,13 +4,13 @@ include_once '../global.php';
 
 class CadastroEspecialidade {
   
-  public function cadastrarNovaEspecialidade($especialidade, $procedimento) {
+  public function cadastrarNovaEspecialidade($especialidade, $procedimento, $percentualDeParticipacao) {
     
     try {      
       Especialidade::getRecordsByField( "especialidade", $especialidade); 
     }
     catch (Exception $e) {
-      $novaEspecialidade = new Especialidade($especialidade, $procedimento);
+      $novaEspecialidade = new Especialidade($especialidade, $procedimento, $percentualDeParticipacao);
       $novaEspecialidade->save();
       echo "Especialidade cadastrada com sucesso!";
     }
