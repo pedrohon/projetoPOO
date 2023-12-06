f353c6550184e37f77aacf9999c644d79e497c70
Sarah <sarahfernandadc@gmail.com>
2023-12-06 08:10:52 -0300

    Implementação da classe Salário

--- Classes/class.Salario.php
+++ Classes/class.Salario.php
@@ -6,8 +6,21 @@ include_once '../global.php';
 class Salario {
 
 
-  public static function calculaParticipacao ($cro, $valorProcedimento, $especialidade, $dataDoSalario) {
-    
-  }
+  public static function calculaParticipacao ($cro, $Procedimento, $especialidade, $dataDoSalario) {
 
+    $objetoespecialidade = Especialidade::getRecordsByField("especialidade", $especialidade);
+    $participacao = $objetoespecialidade[0]->getpercentualDeParticipacao();
+
+    $objetoprocedimento = Procedimento::getRecordsByField("nomeDoProcedimento", $Procedimento);
+    $valorDoProcedimento = $objetoprocedimento[0]->getValorUnitario();
+
+    $valorParaDentista = $participacao * $valorDoProcedimento;
+    
+    $dentista = Dentista::getRecordsByField( "cro", $cro);
+    $salarioAtual = $dentista[0]->getSalario();
+    $salarioNovo = $salarioAtual + $valorParaDentista;
+    
+    $dentista[0]->setSalario($salarioNovo);
+    $dentista[0]->save();
+  }
 }
