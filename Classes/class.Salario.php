bd0019ade1941c907d97fea61fddbc0639d19962
Sarah <114375054+sarahfernandadc@users.noreply.github.com>
2023-12-05 21:00:32 -0300

    Update class.Salario.php

--- Classes/class.Salario.php
+++ Classes/class.Salario.php
@@ -2,20 +2,6 @@
 
 include_once '../global.php'; 
 
-class CalculadoraSalario {
-  public static function calcularSalario($valorProcedimento, $porcentagemParticipacao) {
-      // Verifica se os valores são numéricos
-      if (!is_numeric($valorProcedimento) || !is_numeric($porcentagemParticipacao)) {
-          throw new InvalidArgumentException("Os valores devem ser numéricos.");
-      }
-
-      // Calcula o salário com base nos parâmetros fornecidos
-      $salario = $valorProcedimento * ($porcentagemParticipacao / 100);
-
-      return $salario;
-  }
-}
-
 static class Salario {
 
   public static function calculaParticipacao ($cro, $valorProcedimento, $especialidade, $dataDoSalario) {
