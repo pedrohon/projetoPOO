616dd8911399e8b698efe13740c3f0fba3273444
Pedro Nunes <pedro.nunes02@hotmail.com>
2023-12-06 00:18:59 -0300

    correcao pos merge usuario

--- Classes/class.Usuario.php
+++ Classes/class.Usuario.php
@@ -2,59 +2,28 @@
 
 include_once '../global.php';
 
-// include_once 'persist.php';
+class Usuario extends persist{
 
-class Usuario extends Pessoa { 
-
-  static $local_filename = "Usuario.txt";
-
+    static $local_filename = "Perfil.txt";
   static public function getFilename() {
     return get_called_class()::$local_filename;
   }
 
-  protected $login;
-  protected $senha;
-  protected $idPerfil;
-  protected $logado;
+  private $login;
+  private $senha;
   private static $instancia;
+  private $logado = false;
 
- 
-  public function __construct($nome, $telefone, $email, $cpf, $rg, $login, $senha, $idPerfil) {
-    parent::__construct($nome, $telefone, $email, $cpf, $rg);
+  public function __construct($login, $senha){
     $this->login = $login;
     $this->senha = $senha;
-    $this->idPerfil = $idPerfil;
   }
-  
-   public static function getInstancia($login, $senha){
+
+  public static function getInstancia($login, $senha){
     if (!self::$instancia){
         self::$instancia = new Usuario($login, $senha);
     }
     return self::$instancia;
-  }
-
-  public function getLogin() {
-    return $this->login;
-  }
-
-  public function getSenha() {
-    return $this->senha;
-  }
-
-  public function getIdPerfil() {
-    return $this->idPerfil;
-  }
-
-  public function setLogin($login) {
-    $this->login = $login;
-  }
-
-  public function setSenha($senha) {
-    $this->senha = $senha;
-  }
-
-  public function setIdPerfil($idPerfil) {
-    $this->idPerfil = $idPerfil;
   }
 
   public function realizaLogin($login, $senha){
@@ -68,15 +37,16 @@ class Usuario extends Pessoa {
         }else {
             throw new Exception("Falha no login. Credencial não encontrada.\n");
         }
-      } 
-      catch (Exception $e){
+    } catch (Exception $e){
         echo "Erro: " . $e->getMessage();
-      }
     }
-  
-  public function realizaLogout(){
+    }
+
+    public function realizaLogout(){
         $this->logado = false;
         echo "Logout realizado com sucesso.\n";
     }
-}    
+
+  }
+
 ?>
