c080b6c805c973aedf12a9be1dea53bec7caf002
Pedro Nunes <pedro.nunes02@hotmail.com>
2023-12-06 22:22:05 -0300

    controle de acesso profissional

--- Classes/class.CadastroProfissional.php
+++ Classes/class.CadastroProfissional.php
@@ -1,19 +1,38 @@
 <?php
 
-include_once '../global.php'; 
+include_once '../global.php';
 
 class CadastroProfissional {
   public function cadastrarNovoProfissional($nome, $telefone, $email, $cpf, $rg, $salario, $endereco, $cargo) {
-    try {      
-      Profissional::getRecordsByField( "cpf", $cpf);  
+        $usuarioLogado = $usuario->getLogado();
+        
+        if($usuario->getPerfilDoUsuario() != null)
+        {
+            $existeFuncionalidade = $usuario->getPerfilDoUsuario()->verificaFuncionalidade("Cadastro Profissional");
+        }
+        else{
+            $existeFuncionalidade = false;
+        }
+        if($usuarioLogado && $existeFuncionalidade){
+            try{
+                Profissional::getRecordsByField( "cpf", $cpf);
+                echo "Profissional já cadastrado!\n";
+            }
+            catch(Exception $e){
+                $novoProfissional = new Profissional($nome, $telefone, $email, $cpf, $rg, $salario, $endereco, $cargo);
+                $novoProfissional->salvarProfissional();
+                echo "Profissional cadastrado com sucesso!\n";
+            }
+        }
+        else{
+            if(!$usuarioLogado){
+                echo "\nErro: Não foi possível realizar o cadastro do profissional porque o usuário '" . $usuario->getLogin() . "' não está logado!\n\n";
+            }
+            else{
+                echo "\nErro: Não foi possível realizar o cadastro do profissional porque o usuário '" . $usuario->getLogin() . "' não tem permissão para cadastrar profissionais!\n\n";
+            }
+        }
     }
-    catch (Exception $e) {
-      $novoProfissional = new Profissional($nome, $telefone, $email, $cpf, $rg, $salario, $endereco, $cargo);
-      $novoProfissional->salvarProfissional();
-      echo "Profissional cadastrado com sucesso!\n";
-
-    }
-  }
 }
 
 //para usar 
